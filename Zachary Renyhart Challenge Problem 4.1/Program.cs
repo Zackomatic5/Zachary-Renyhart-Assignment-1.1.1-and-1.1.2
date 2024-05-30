c09ae135c33a662e34bc9c0fc0c94009c2418a43
Zachary Renyhart <zachary.renyhart@outlook.com>
2024-05-30 12:12:00 -0400

    Zachary Renyhart Challenge 4.1

--- Zachary Renyhart Challenge Problem 4.1/Program.cs
+++ Zachary Renyhart Challenge Problem 4.1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Zachary_Renyhart_Challenge_Problem_4._1
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Zachary_Renyhart_Challenge_Problem_4._1
 {
     internal class Program
     {
@@ -10,20 +12,95 @@
             string customerName = Console.ReadLine();
             Console.WriteLine("Hello " + customerName + "," + " please enter the unit amount you consumed: ");
             double UnitUsed = Convert.ToDouble(Console.ReadLine());
-            double num = 0;
-         switch (UnitUsed)
+
+            switch (UnitUsed)
             {
-                case > 199:
-                    
-                    MyMethodOne(num, UnitUsed);
+                case <= 199:
+                    double num = 1.20;
+                    Console.WriteLine($"Amount Charges @ $1.20 a unit are $ {MyMethodOne(num, UnitUsed)}");
+                    Console.WriteLine();
+                    break;
+
+                case <= 399:
+                    double num2 = 1.50;
+                    Console.WriteLine($"Amount charges @ $1.50 a unit are $ {MyMethodTwo(num2, UnitUsed)}");
+                    Console.WriteLine();
                 break;
+
+                case <= 599:
+                    double num3 = 1.80;
+                    Console.WriteLine($"Amount charges @ $1.80 a unit are $ {MyMethodThree(num3, UnitUsed)}");
+                    Console.WriteLine();
+                break;
+
+                case > 600:
+                    double num4 = 2.00;
+                    Console.WriteLine($"Amount charges @ $2.00 a unit are $ {MyMethodTwo(num4, UnitUsed)}");
+                    Console.WriteLine();
+                break;
+
+
             }          
 
         }
         static double MyMethodOne(double num, double unitUsed)
         {
+            Console.WriteLine($"Your unit used was {unitUsed}");
             num = 1.20;
             double total = num * unitUsed;
+            return total;
+        }
+        static double MyMethodTwo(double num, double unitUsed)
+        {
+            num = 1.50;
+            double total = num * unitUsed;
+            if (total > 400)
+            {
+                Console.WriteLine($"Your unit used was: {unitUsed}");
+                Console.WriteLine($"Your initial charge $ {total}");
+                double percentage = 0.15;
+                double grandTotal = total * percentage;
+                total = total + grandTotal;
+                
+                Console.WriteLine($"Your surcharge was $ {grandTotal}");
+               
+            }
+            return total;
+        }
+
+        static double MyMethodThree(double num, double unitUsed)
+        {
+            num = 1.80;
+            double total = num * unitUsed;
+            if (total > 400)
+            {
+                Console.WriteLine($"Your unit used was: {unitUsed}");
+                Console.WriteLine($"Your initial charge $ {total}");
+                double percentage = 0.15;
+                double grandTotal = total * percentage;
+                total = total + grandTotal;
+
+                Console.WriteLine($"Your surcharge was $ {grandTotal}");
+
+            }
+            return total;
+        }
+
+        static double MyMethodFour(double num, double unitUsed)
+        {
+            num = 2.00;
+            double total = num * unitUsed;
+            if (total > 400)
+            {
+                Console.WriteLine($"Your unit used was: {unitUsed}");
+                Console.WriteLine($"Your initial charge $ {total}");
+                double percentage = 0.15;
+                double grandTotal = total * percentage;
+                total = total + grandTotal;
+
+                Console.WriteLine($"Your surcharge was $ {grandTotal}");
+
+            }
             return total;
         }
     }    
