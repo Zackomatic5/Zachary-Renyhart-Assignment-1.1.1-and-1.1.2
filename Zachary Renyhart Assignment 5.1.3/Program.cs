385a3a156244937f9715c386bd4156637b372b3a
Zachary Renyhart <zachary.renyhart@outlook.com>
2024-06-03 16:50:03 -0400

    Zachary Renyhart Assignment 5.1.3

---
+++ Zachary Renyhart Assignment 5.1.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Zachary_Renyhart_Assignment_5._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 2, 3, 5, 6 };
+            Dictionary<int, int> count = new();
+            foreach (int num in array) 
+            {
+                if (count.ContainsKey(num))
+                {   //This checks to see if the dictionary already has the numbers in the array
+                    break;
+                }    
+                else
+                {
+                    //If its not in the dictionary it adds a 1 value to it.
+                    //The key is the number in the array. The value is 1 if its the first time it populates
+                    count[num] = 1;
+                }
+                
+            }           
+            Console.WriteLine("duplicate numbers");                        
+        }
+       
+
+    }
+}
---
+++ Zachary Renyhart Assignment 5.2.1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Zachary_Renyhart_Assignment_5._2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence");
+            string sentence = Console.ReadLine();
+            //This splits the strings into words
+            string[] Result = sentence.Split(' ');
+            //This is showing that the array length of result is greater than 0
+            if (Result.Length > 0)
+            {
+                Console.WriteLine($"The number of letters in the last word is: {Result[Result.Length - 1].Length}");
+            }
+                
+                    
+                
+                 
+            
+            
+
+
+        }
+    }
+}
